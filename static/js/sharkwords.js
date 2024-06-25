b60f8f68663e43ab7c77ec1c2b217e74499b1764
HelenEdwards <hgedwards87@gmail.com>
2024-06-25 12:55:55 -0700

    done with part 1

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -29,15 +29,19 @@ const word = 'hello';
 //
 // Use the following template string to create each div:
 // `<div class="letter-box ${letter}"></div>`
-//
+//  #word-container
 function createDivsForChars() {
-  // Replace this with your code
+  for (const letter of word) {
+    document.querySelector('#word-container').insertAdjacentHTML('beforeend', `<div class=letter-box ${letter}></div>`);
+  }
 }
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons".
 function generateLetterButtons() {
-  // Replace this with your code
+  for (const char of ALPHABET) {
+    document.querySelector('#letter-buttons').insertAdjacentHTML('beforeend', `<button>${char}</button>`)
+  }
 }
 
 // Set the `disabled` property of `buttonEl` to true.
@@ -45,14 +49,18 @@ function generateLetterButtons() {
 // `buttonEl` is an `HTMLElement` object.
 //
 function disableLetterButton(buttonEl) {
-  // Replace this with your code
+  buttonEl.disabled = true
 }
 
 // This is a helper function we will use in the future
 // It should return `true` if `letter` is in the word
 // For now, you should test it out to make sure it works
 function isLetterInWord(letter) {
-  // Replace this with your code
+  // console.log(word[letter])
+  if (word.includes(letter)) {
+    return true;
+  }
+  return false;
 }
 
 // This function is called to start the game.
